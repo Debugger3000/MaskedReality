667a22cdbfb4757c1a2205b7e1e355ed1a062052
Debugger3000 <145146370+Debugger3000@users.noreply.github.com>
2026-02-01 13:20:34 -0500

    sprites-turret,portal,projectile

--- Assets/Scripts/WorldHazardController.cs
+++ Assets/Scripts/WorldHazardController.cs
@@ -4,14 +4,27 @@ using UnityEngine.SceneManagement;
 public class WorldHazardController : MonoBehaviour
 {
     public GameObject onDeathEffectBlood;
+    public AudioSource audioSource;
+    public AudioClip deathClip;
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            //play death audio
+            if (deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+
             Destroy(other.gameObject);
+
+            
+
             //play blood prefab or particle effect here
             Instantiate(onDeathEffectBlood, other.gameObject.transform.position, Quaternion.identity);
+
+            
 
             Invoke("Kill", 1f);
         }
